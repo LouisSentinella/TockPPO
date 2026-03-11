7d411d598c8ce93cc606254d5fbd618c04c7d816
Louis Sentinella <louissentinella@gmail.com>
2026-03-11 22:33:39 +0000

    Added game logic for every card except 7 (scary recursive logic)

--- rules.py
+++ rules.py
@@ -1,4 +1,4 @@
-from action import Action
+from action import Action, ActionType
 from board import Board
 from state import Zone, Pawn, GameState
 from typing import Optional
@@ -63,6 +63,31 @@ def build_tile_map(state: GameState) -> dict[int, Optional[Pawn]]:
 
     return tile_map
 
+def is_path_blocked(path, tile_map, board):
+    for tile in path:
+        if tile_map[tile] is not None and board.is_just_out(tile_map[tile]):
+            return True
+    return False
+
+def add_normal_move_actions(card, pawn, board, tile_map, state, actions, steps=None):
+    steps = steps or card
+
+    try:
+        path = board.get_path(pawn, steps)
+    except ValueError:
+        return
+
+    if not is_path_blocked(path, tile_map, board):
+        actions.append(Action(card=card, action_type=ActionType.MOVE, pawn=pawn, path=path, enter_home=False))
+
+    if board.HOME_ENTRY_TILES[pawn.owner] in path:
+        try:
+            home_path = board.get_path(pawn, steps, enter_home=True)
+            if not is_path_blocked(home_path, tile_map, board):
+                actions.append(Action(card=card, action_type=ActionType.MOVE, pawn=pawn, path=home_path, enter_home=True))
+        except ValueError:
+            pass
+
 def get_legal_moves(state: GameState, board: Board) -> list[Action]:
     current_player = state.active_player
     player_hand = state.hands[current_player]
@@ -70,21 +95,49 @@ def get_legal_moves(state: GameState, board: Board) -> list[Action]:
     actions = []
     tile_map = build_tile_map(state)
 
+    player_pawns = state.pawns[current_player]
+    other_player_index = [p for p in range(3) if p != current_player]
+    other_pawns = [j for i in other_player_index for j in state.pawns[i]]
+    base_pawns = [p for p in player_pawns if p.zone == Zone.BASE]
+
     for card in set(player_hand):
         if card == 1:
-            pass
-        elif card in [2, 3, 6, 8, 9, 11, 12]:
-            pass
+            if len(base_pawns) > 0:
+                just_out_occupant = tile_map[board.JUST_OUT_TILES[current_player]]
+                if just_out_occupant is None or just_out_occupant.owner != current_player:
+                    actions.append(Action(card=card, action_type=ActionType.DEPLOY))
+
+            for pawn in [p for p in player_pawns if p.zone != Zone.BASE]:
+                add_normal_move_actions(1, pawn, board, tile_map, state, actions, steps=1)
+                add_normal_move_actions(1, pawn, board, tile_map, state, actions, steps=11)
+        elif card in [2, 3, 6, 8, 9, 10, 12]:
+            for pawn in [p for p in player_pawns if p.zone != Zone.BASE]:
+                add_normal_move_actions(card, pawn, board, tile_map, state, actions)
         elif card == 4:
-            pass
+            for pawn in [p for p in player_pawns if p.zone == Zone.MAIN]:
+                path = board.get_path_backward(pawn, 4, )
+                if not is_path_blocked(path, tile_map, board):
+                    actions.append(
+                        Action(card=card, action_type=ActionType.MOVE, pawn=pawn, path=path, enter_home=False))
+
         elif card == 5:
-            pass
+            for pawn in [p for j in state.pawns for p in j if p.zone != Zone.BASE and not board.is_just_out(p)]:
+                add_normal_move_actions(card, pawn, board, tile_map, state, actions)
         elif card == 7:
             pass
-        elif card == 10:
-            pass
+        elif card == 11:
+            for pawn in [p for p in player_pawns if p.zone == Zone.MAIN and not board.is_just_out(p)]:
+                for other_pawn in [p for p in other_pawns if p.zone == Zone.MAIN and not board.is_just_out(p)]:
+                    actions.append(Action(card=card, action_type=ActionType.SWAP, pawn=pawn, target_pawn=other_pawn))
         elif card == 13:
-            pass
+            if len(base_pawns) > 0:
+                just_out_occupant = tile_map[board.JUST_OUT_TILES[current_player]]
+                if just_out_occupant is None or just_out_occupant.owner != current_player:
+                    actions.append(Action(card=card, action_type=ActionType.DEPLOY))
+
+            for pawn in [p for p in player_pawns if p.zone != Zone.BASE]:
+                add_normal_move_actions(card, pawn, board, tile_map, state, actions)
+
         else:
             raise Exception(f'Unknown card {card}')
 
